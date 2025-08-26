c5e75999939012fd01d4bc42a34b29631a76c8d2
luckt-arya <lklk112111@gmail.com>
2025-08-26 19:15:39 +0530

    Add a new section global intitaive and profie photo in reviews section

--- script.js
+++ script.js
@@ -37,21 +37,21 @@ window.addEventListener("scroll", () => {
 });
 
 // Fade-in on scroll (IntersectionObserver)
-const faders = document.querySelectorAll(".fade-in");
-const appearOptions = {
-  threshold: 0.2,
-};
-const appearOnScroll = new IntersectionObserver((entries, observer) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("visible");
-    }
-  });
-}, appearOptions);
+// const faders = document.querySelectorAll(".fade-in");
+// const appearOptions = {
+//   threshold: 0.2,
+// };
+// const appearOnScroll = new IntersectionObserver((entries, observer) => {
+//   entries.forEach((entry) => {
+//     if (entry.isIntersecting) {
+//       entry.target.classList.add("visible");
+//     }
+//   });
+// }, appearOptions);
 
-faders.forEach((fader) => {
-  appearOnScroll.observe(fader);
-});
+// faders.forEach((fader) => {
+//   appearOnScroll.observe(fader);
+// });
 
 const countries = [
   { code: "in", name: "India" },
